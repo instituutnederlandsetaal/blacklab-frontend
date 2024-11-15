725679edf7e1d1bcef3a59c95e4aa68cdc8e79af
Jan Niestadt <jan.niestadt@ivdnt.org>
2024-11-15 10:20:33 +0100

    Improve error message if BlackLab wasn't found.
    If an ApiError with httpCode 404 occurs during initialization,
    either BlackLab isn't running, or corpus-frontend's properties file
    isn't correctly configured. The new error message indicates this
    and directs you to the online documentation.

--- src/frontend/src/store/search/index.ts
+++ src/frontend/src/store/search/index.ts
@@ -370,10 +370,20 @@ const init = async () => {
 				privateActions.setLoadingState({loadingState: 'requiresLogin', loadingMessage: e.message});
 			} else if (e.httpCode === 403) {
 				privateActions.setLoadingState({loadingState: 'unauthorized', loadingMessage: e.message});
+			} else if (e.httpCode === 404) {
+				// Not found. May not be configured correctly.
+				privateActions.setLoadingState({loadingState: 'error', loadingMessage:
+					'Unable to contact BlackLab Server (or corpus-frontend\'s own server component). ' +
+					'Make sure both .war applications have been deployed, and your properties file ' +
+					'is in the correct location and has the correct name. ' +
+					'Refer to the documentation at https://github.com/INL/corpus-frontend '
+				});
 			} else {
+				// Some other API error. Show message.
 				privateActions.setLoadingState({loadingState: 'error', loadingMessage: e.message});
 			}
 		} else {
+			// Non-API error. Show message.
 			privateActions.setLoadingState({loadingState: 'error', loadingMessage: e.message ?? e.toString()});
 		}
 		return false;
